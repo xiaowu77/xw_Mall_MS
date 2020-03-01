b5f9f231a3e489b7a47aa8eb1df52f892fbd62cd
xiaowu77 <2678713877@qq.com>
2020-03-01 21:10:47 +0800

    用户权限管理完成部分

---
+++ src/components/Power/rights.js
@@ -0,0 +1,27 @@
+var rights=[
+  {
+    authName:'商品管理',
+    path: 'goods',
+    level: '0'
+  },
+  {
+    authName:'订单管理',
+    path: 'order',
+    level: '0'
+  },
+  {
+    authName:'权限管理',
+    path: 'rights',
+    level: '0'
+  },
+  {
+    authName:'商品列表',
+    path: 'goods',
+    level: '1'
+  },
+  {
+    authName:'添加商品',
+    path: 'goods',
+    level: '2'
+  },
+]
